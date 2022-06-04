c10a768527fed9e681946a916499fafe0dfd2c14
dcanasp <95656381+dcanasp@users.noreply.github.com>
2022-06-04 02:18:10 -0500

    reorganizar y dejar de ulilizar casi todo

---
+++ frontend/comptech/src/pages/Home/Principal.jsx
@@ -0,0 +1,12 @@
+import MainImage from "../../components/MainImage.js";
+import Computadoras from "../../components/Computadoras";
+
+
+export default function Principal (){
+return(
+    <div>
+    <MainImage/>
+    <Computadoras/>
+    </div>
+);
+};
