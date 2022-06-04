2ffd5870ede5eba678d13864bfdab67d8c25e5cd
dcanasp <95656381+dcanasp@users.noreply.github.com>
2022-06-04 02:17:36 -0500

    reorganizar y dejar de ulilizar casi todo

--- frontend/comptech/src/App.js
+++ frontend/comptech/src/App.js
@@ -1,16 +1,22 @@
 import NavBar from './common/navbar/NavBar';
+import Cabezera from './common/navbar/Cabezera';
 import './App.css';
 import Home from './pages/Home/Home';
+import Principal from './pages/Home/Principal';
+import Footer from './common/Footer';
 import { BrowserRouter  as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
- 
     <Router basename={process.env.PUBLIC_URL}>
-      <NavBar />
-      <Routes>
-      <Route path="/" element={<Home />} />     
+      <Cabezera></Cabezera>
+      {/* <NavBar />  */}
+       <Routes>
+      {/* <Route path="/" element={<Home />} />      */}
+      <Route path="/" element={<Principal></Principal>}></Route>
        </Routes>
+   <div/>
+    <Footer></Footer>
     </Router>
        
   );
