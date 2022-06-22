491b0e99ce2e9db6ff9809df066c3b4050e30929
Steven-Rodriguez-git <steven_980213@hotmail.com>
2022-06-21 20:47:52 -0500

    push

--- coneccionMongo/coneccionMongo/src/main/java/coneccionFront/Main.java
+++ coneccionMongo/coneccionMongo/src/main/java/coneccionFront/Main.java
@@ -1,22 +1,8 @@
 package coneccionFront;
-
-import com.mycompany.coneccionmongo.arbolesPorComponente;
 import com.mycompany.coneccionmongo.principal;
-
-import coneccionFront.GetService;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Objects;
 import java.util.concurrent.ExecutionException;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
-
 
 /**
  *
